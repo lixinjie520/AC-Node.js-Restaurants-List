1a002f73c47f7a45952e812afb4c8fc798fb5ced
Amber Wang <wangshengyan6@gmail.com>
2024-04-10 16:48:31 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// 根路徑會重新導航到網站首頁
 app.get("/", (req, res) => {
-    res.send("express app for restaurants");
+    res.redirect("/restaurants");
+});
+
+// 設定網站首頁路由
+app.get("/restaurants", (req, res) => {
+    res.send("home page of restaurants");
+});
+
+// 設定餐廳詳細資料路由
+app.get("/restaurant/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`restaurant: ${id}`);
 });
 
 app.listen(port, () => {
